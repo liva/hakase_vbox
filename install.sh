54c1335fcc3e9c6b8fa46b38031050f1efe731ee
liva <sap.pcmail@gmail.com>
2018-06-04 01:08:19 +0900

    improve installer scripts

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
-#!/bin/bash -x
+#!/bin/bash
+set -e
 
-cd /usr/src
+if [ "$EXEC_PROTECTOR" != "disabled" ]; then
+    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+    echo "DO NOT RUN install.sh directly!!"
+    echo "USE setup.sh INSTEAD!"
+    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+    exit 1
+fi
+
+# avoid duplicate installation
+test -f /etc/hakase_installed && exit
+
+pushd /usr/src
 su -c "grep '^deb ' /etc/apt/sources.list | sed 's/^deb/deb-src/g' > /etc/apt/sources.list.d/deb-src.list"
 sed -i'~' -E "s@http://(..\.)?(archive|security)\.ubuntu\.com/ubuntu@http://linux.yz.yamagata-u.ac.jp/pub/linux/ubuntu-archive/@g" /etc/apt/sources.list
 
@@ -15,3 +27,6 @@ dpkg -i *hakase-1_amd64.deb
 
 sed -i -e "s/GRUB_CMDLINE_LINUX_DEFAULT=\"quiet\"/GRUB_CMDLINE_LINUX_DEFAULT=\"quiet memmap=0x80000000\$0x80000000\"/g" /etc/default/grub
 update-grub2
+popd
+
+date > /etc/hakase_installed
--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
-#!/bin/bash -x
-vagrant plugin install vagrant-vbguestv
-agrant up
-vagrant ssh -c "sudo bash -x /vagrant/install.sh"
+#!/bin/bash
+set -e
+
+vagrant destroy -f || :
+vagrant plugin install vagrant-vbguest
+trap 'vagrant destroy -f' SIGINT ERR
+vagrant up
+vagrant ssh -c "sudo env EXEC_PROTECTOR=disabled bash /vagrant/install.sh"
 vagrant reload
+
+echo "setup done!"
