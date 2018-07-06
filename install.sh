f8c735b0a05639765249bb3c7c216d7c54b6f13f
liva <sap.pcmail@gmail.com>
2018-07-07 02:38:19 +0900

    fix kernel boot parameter

--- install.sh
+++ install.sh
@@ -39,7 +39,7 @@ if [ -e /etc/default/grub.d/50-cloudimg-settings.cfg ]; then
     GRUB_CONFIG="/etc/default/grub.d/50-cloudimg-settings.cfg"
 fi
 
-sed -i -e "s/GRUB_CMDLINE_LINUX_DEFAULT=\"/GRUB_CMDLINE_LINUX_DEFAULT=\"memmap=0x80000000\\\\\\\$0x80000000 /g" ${GRUB_CONFIG}
+sed -i -e "s/GRUB_CMDLINE_LINUX_DEFAULT=\"/GRUB_CMDLINE_LINUX_DEFAULT=\"memmap=0x80000000\\\\\\\\\\\\\$0x80000000 /g" ${GRUB_CONFIG}
 sed -i -e "s/GRUB_TIMEOUT=10/GRUB_TIMEOUT=2/g" ${GRUB_CONFIG}
 update-grub2
 popd
